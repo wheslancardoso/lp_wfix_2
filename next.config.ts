5a516181be1407657c867d79f8fb6b18d3e858ee
wheslancardoso <wheslancardoso1@gmail.com>
2026-05-19 13:10:30 -0300

    chore: configura qualidades permitidas de imagem no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
     formats: ['image/avif', 'image/webp'],
+    qualities: [75, 85],
     remotePatterns: [
       {
         protocol: 'https',
